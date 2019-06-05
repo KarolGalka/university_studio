23aefddbe2bc12f9dcf328e54402ed2e5318c771
KarolGalka <karolgalka96@o2.pl>
2019-06-05 22:50:11 +0200

    Implemented Newton NLP

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ TWITTER_KEYS = {"consumer_key": 'bifP4IUwhlU1O75ScL5VZonzv',
   "consumer_secret": 'CLNuiGB85ew1XFqsJFns6rimdVRaWbjXqaVErIxAZ1DQunWyVe',
   "access_token_key": '893562608-8qtBCIMoqwiXEcApYBWppnIR2O3pJXJkDLJOuqOU',
   "access_token_secret": 'IAga4TnAlfoYeMDObDDKXb5uMDRIN7yrPh1owL7RridBy'}
+
+IBM_API_KEY = "cbGnqfMQ7IF0rLqOo9uygpT0MoSjWvzTL0Oh2f6XB-kC"
+IBM_API_URL = "https://gateway-lon.watsonplatform.net/natural-language-understanding/api"
---
+++ data_algorithms/nlp.py
@@ -0,0 +1,28 @@
+
+import requests
+# import config
+
+'''
+Source: https://cloud.ibm.com/docs/services/natural-language-understanding?topic=natural-language-understanding-getting-started#getting-started-tutorial
+Used https://curl.trillworks.com to translate curl from source
+'''
+# TODO use config 
+IBM_API_KEY = "cbGnqfMQ7IF0rLqOo9uygpT0MoSjWvzTL0Oh2f6XB-kC" 
+IBM_API_URL = "https://gateway-lon.watsonplatform.net/natural-language-understanding/api"
+
+def get_categories_from_text(text):
+    s = requests.Session()
+    headers= {"Content-Type": "application/json"}
+    params = (
+        ('version', '2018-11-16'),
+    )
+    data = '{\n  "text": "' + text + '",\n  "features": {\n    "categories": {\n    }\n    }\n}'
+    # data = '{\n  "text": "I love apples! I do not like oranges.",\n  "features": {\n    "categories": {\n    }\n    }\n}'
+    s.headers.update(headers)
+    r = requests.post('https://gateway-lon.watsonplatform.net/natural-language-understanding/api/v1/analyze', headers=headers, params=params, data=data, auth=('apikey', IBM_API_KEY))
+    # r = s.post(IBM_API_URL)#config.IBM_API_URL)
+    print(r.text)
+
+text = "I like sport, mostly football. Mohamed Salah is my favorite player. I like going to gym aswell"
+get_categories_from_text(text)
+print(0)
